1263179bae85cca42100f9196a9ecca4334ced11
Hussain Jamal <Hjamal9865@gmail.com>
2025-08-25 15:34:08 +0500

    header search bar

--- api/routes/listingRoutes.js
+++ api/routes/listingRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyToken.js'
-import { createListing , deleteListing ,getListing,updateListing} from '../controllers/listingController.js';
+import { createListing , deleteListing ,getListing,getListings,updateListing} from '../controllers/listingController.js';
 
 const router = express.Router()
 
@@ -8,7 +8,7 @@ router.post('/create', verifyToken, createListing)
 router.delete('/delete/:id', verifyToken, deleteListing);
 router.post('/update/:id', verifyToken, updateListing);
 router.get('/get/:id', getListing);
-
+router.get('/get', getListings);
 
 
 export default router;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,30 @@
 import { useState } from "react";
 import { Menu, X, Search, Building } from "lucide-react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
   const { currentUser } = useSelector((state) => state.user);
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
 
   return (
     <nav className="bg-white shadow-md fixed top-0 left-0 w-full z-50">
@@ -19,16 +38,20 @@ export default function Navbar() {
           </div>
 
           <div className="hidden md:flex flex-1 justify-center">
+          <form onSubmit={handleSubmit}>
             <div className="flex w-full max-w-md">
               <input
                 type="text"
                 placeholder="Search properties..."
+                value={searchTerm}
+                onChange={(e) =>setSearchTerm(e.target.value)}
                 className="w-full px-4 py-2 rounded-l-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
-              <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 rounded-r-full flex items-center justify-center transition">
+              <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 rounded-r-full flex items-center justify-center border-none transition">
                 <Search size={18} />
               </button>
             </div>
+                </form>
           </div>
 
           {/* Links */}
