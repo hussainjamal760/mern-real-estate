a5fd85873540f60f9ec43403361de350fd8fdf99
Hussain Jamal <Hjamal9865@gmail.com>
2025-08-26 10:55:58 +0500

    deploying error 2 fixed

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import {useDispatch, useSelector} from 'react-redux'
 import {signInFailure,signInStart,signInSuccess} from '../redux/user/userSlice.js'
-import OAuth from "../components/oAuth.jsx";
+import OAuth from "../components/OAuth.jsx";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
