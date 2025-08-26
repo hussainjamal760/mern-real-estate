894a6360a6e0041376323e9c34cfff09f326e36a
Hussain Jamal <Hjamal9865@gmail.com>
2025-08-26 11:46:59 +0500

    path for lottiefiles changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -139,7 +139,7 @@ export default function Home() {
 <div className="flex-1 flex justify-center lg:justify-end z-10">
   <div className="relative">
     <DotLottieReact
-      src="/src/assets/hero.json"
+      src="./public/hero.json"
       loop
       autoplay
       style={{
